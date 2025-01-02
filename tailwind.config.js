c91d30d8ca774fbd00fe7406480a5f4fed17828f
felicia <feli5659@stud.kea.dk>
2025-01-02 22:59:03 +0700

    hero almost there

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: ["./index.html", "./netlify/functions/*.js", "assets"],
   theme: {
     colors: {
+      white: "#ffffff",
       creme: "#f4ebe2",
       "light-creme": "#f8f7f4",
       "dark-creme": "#cec3b2",
